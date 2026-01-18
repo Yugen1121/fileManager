234f2ed7267582261455c4eebcb7caf29559aeb6
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-18 11:51:48 +0000

    Added a data handler class with constructore to read data from the resource json file

---
+++ FileManager/src/main/java/FileManager/FileManager/DataHandler.java
@@ -0,0 +1,38 @@
+package FileManager.FileManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+import java.io.FileReader;
+
+import java.nio.file.Paths;
+import java .nio.file.Path;
+
+import java.lang.reflect.Type;
+
+import java.util.Map;
+
+public class DataHandler {
+	
+	private Map<String, Map<String, String>> data;
+	
+	public DataHandler() throws Exception{
+		
+			// Reading the data from the json file
+			FileReader reader = new FileReader("src/main/resources/data.json");
+			
+			// Initialising Gson
+			Gson gson = new Gson();
+			
+			// Creating a type for the data 
+			Type type = new TypeToken<Map<String, Map<String, String>>>(){}.getType();
+			
+			this.data = gson.fromJson(reader, type);
+			
+			// closing reader
+			reader.close();
+		
+		
+	}
+}
