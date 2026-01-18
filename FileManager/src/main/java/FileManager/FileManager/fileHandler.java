83a64c33d04003617fc842c2d61004aa2dd5a41e
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-18 13:32:37 +0000

    added a function to rename file

--- FileManager/src/main/java/FileManager/FileManager/fileHandler.java
+++ FileManager/src/main/java/FileManager/FileManager/fileHandler.java
@@ -17,8 +17,30 @@ public class fileHandler {
 		Path target = Paths.get(tgt);
 		
 		// Atomically moves the file
-		// Throw file not found exception if target doesn't exist
+		// Throw file not found exception 	if target doesn't exist
 		Files.move(source, target, StandardCopyOption.ATOMIC_MOVE);
+	}
+	
+	public static void renameFile(String path, String name) throws Exception{
+		// Source path
+		Path source = Paths.get(path);
+		
+		// throw exception if source file doesn't exist 
+		if(!Files.exists(source)) {
+			throw new RuntimeException("File doesn't exist.");
+		}
+		
+		// parent directory
+		Path parent = source.getParent();
+		
+		// Target path
+		Path target = parent.resolve(name);	
+		
+		// Atomically moves the file
+		// Throw file not found exception 	if target doesn't exist
+		Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+		
+		
 	}
 }
 	
