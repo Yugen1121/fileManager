b34479d3f4fb8a3360bcd31a23c11d7774be40ad
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-19 23:50:59 +0000

    Added some even listeners and updated code to handle the updated made in core classes

--- FileManager/src/main/java/FileManager/FileManager/App.java
+++ FileManager/src/main/java/FileManager/FileManager/App.java
@@ -1,13 +1,65 @@
 package FileManager.FileManager;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import FileManager.FileManager.components.Card;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
+
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	launch(args);
     }
+    
+    public App() {
+    	
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// root component to go to the root
+		StackPane root = new StackPane();
+		
+		// initialise scene
+		Scene scene = new Scene(root, 400, 400);
+		
+		// initialise a main component
+		HBox main = new HBox();
+		
+		root.getChildren().add(main);
+		
+		VBox left = new VBox();
+		VBox right = new VBox();
+		
+		ObservableList<Node> leftChildren = left.getChildren();
+		
+		// initialise data handler
+		DataHandler dh = new DataHandler();
+		System.out.println(dh.getData());
+		// add path cards to the GUI
+		for (String pth: dh.getData().keySet()) {
+			FileWatcher d = new FileWatcher(pth, dh);
+			Card c = new Card(d, right);
+			leftChildren.add(c.getRoot());
+		}
+		System.out.println(leftChildren);
+		
+		main.getChildren().add(left);
+		main.getChildren().add(right);
+		System.out.println(leftChildren);
+		primaryStage.setTitle("Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
 }
--- FileManager/src/main/java/FileManager/FileManager/components/Card.java
+++ FileManager/src/main/java/FileManager/FileManager/components/Card.java
@@ -1,15 +1,61 @@
 package FileManager.FileManager.components;
 
+import FileManager.FileManager.FileWatcher;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
 
-public class Card {
-	public static VBox getCard(String path) { 
-		VBox root = new VBox();
-		HBox name = new HBox();
-		name.getChildren().add(new Label(path));
-		root.getChildren().add(name);
-		return root;
+public class Card{
+	private VBox mainSection;
+	private FileWatcher fw;
+	private VBox root;
+	
+	public Card(FileWatcher fw, VBox right) { 
+		this.root = new VBox();
+		this.fw = fw;
+		this.mainSection = right; 
+		build();
 	}
+	
+	public VBox getRoot() {
+		return this.root;
+	}
+	
+	private void build() {
+		this.root.getChildren().clear();
+		HBox name = new HBox();
+		name.getChildren().add(new Label(fw.getName().get()));
+		name.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			buildMainSection();
+		});
+		
+		root.getChildren().add(name);
+	}
+	
+	private void buildMainSection() {
+		mainSection.getChildren().clear();
+		fw.getFilePaths().addListener((MapChangeListener<String, String>) change -> {
+			buildMainSection();
+		});
+		ObservableMap<String, String> map = fw.getFilePaths(); 
+		for (String k: map.keySet()) {
+			mainSection.getChildren().add(new Label(k+": "+map.get(k)));
+		}
+		TextField fileTypeInput = new TextField();
+		TextField filePath  = new TextField();
+		mainSection.getChildren().add(fileTypeInput);
+		mainSection.getChildren().add(filePath);
+		Button btn = new Button("Submit");
+		btn.setOnAction(e -> {
+			fw.addFilePath(fileTypeInput.getText(), filePath.getText());
+		});
+		mainSection.getChildren().add(btn);
+	}
+	
 }
