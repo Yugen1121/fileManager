ef573a78d4b03f7247e578edf72bcae0425e562c
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-19 22:19:06 +0000

    added a function to add/update filepath into the json and inmemory datastructure

--- FileManager/src/main/java/FileManager/FileManager/FileWatcher.java
+++ FileManager/src/main/java/FileManager/FileManager/FileWatcher.java
@@ -8,18 +8,27 @@ import javafx.collections.FXCollections;
 public class FileWatcher {
 	private ObservableMap<String, String> filePath = FXCollections.observableHashMap();
 	private String directoryName;
+	private DataHandler dh;
 	
-	public FileWatcher(String name, ObservableMap<String, String> map) {
+	public FileWatcher(String name, ObservableMap<String, String> map, DataHandler dh) {
 		this.filePath = map;
 		this.directoryName = name;
+		this.dh = dh;
 	}
 	
-	public String directoryName() {
+	public String getDirectoryName() {
 		return this.directoryName;
 	}
 	
+	// gets all file path
 	public ObservableMap <String, String> getFilePaths(){
 		return this.filePath;
+	}
+	
+	// adds a new file path 
+	public void addFilePath(String fileType, String path) {
+		this.filePath.put(fileType, path);
+		this.dh.updateData();
 	}
 	
 }
