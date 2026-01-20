8a1b13474096c1fb7ed412d48445d6b66ddaa4f8
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-20 21:04:48 +0000

    added function to create directoryWatcher

--- FileManager/src/main/java/FileManager/FileManager/FileWatcher.java
+++ FileManager/src/main/java/FileManager/FileManager/FileWatcher.java
@@ -1,10 +1,14 @@
 package FileManager.FileManager;
 
-import com.darylteo.nio.*;
+
 
 import javafx.collections.ObservableMap;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import io.methvin.watcher.DirectoryWatcher;
 
 public class FileWatcher {
 	private String directoryPath;
@@ -15,6 +19,60 @@ public class FileWatcher {
 		this.directoryPath = path;
 		this.dh = dh;
 		this.config = dh.getData().get(path);
+		if (config.getActive().get() == true) {
+			try {
+				this.startFileWatcher();
+			}
+			catch (Exception e) {
+				System.out.println("Failed to start watcher for: " + this.directoryPath);
+			}
+		}
+	}
+	
+	public void startFileWatcher() throws Exception{
+		Path p = Paths.get(directoryPath).toAbsolutePath().normalize();
+		System.out.println(p.toString());
+		DirectoryWatcher watcher = DirectoryWatcher.builder()
+				.path(p)
+				.listener(e -> {
+					System.out.println("0");
+					switch (e.eventType()) {
+					case CREATE: {
+						try {
+							System.out.println(1);
+							Thread.sleep(500);
+							Path iP = e.path();
+							String sP= iP.toString();
+							int ind = sP.lastIndexOf(".");
+							String type = sP.substring(ind+1);
+							if (config.getFilePaths().containsKey(type)) {
+								fileHandler.moveFile(sP, config.getFilePaths().get(type));
+								System.out.println("Successful");
+							}
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+						
+					}
+					
+					default:
+						throw new IllegalArgumentException("Unexpected value: ");
+					}
+				}).build();
+		Thread watcherThread = new Thread(()->{
+			try {
+				watcher.watch();
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		});
+		watcherThread.setDaemon(true);
+		watcherThread.start();
 	}
 	
 	public String getDirectoryPath() {
@@ -35,7 +93,7 @@ public class FileWatcher {
 		this.config.getFilePaths().put(fileType, path);
 		this.dh.updateData();
 	}
-	
+
 	public synchronized void removeFilePath(String fileType) {
 		this.config.getFilePaths().remove(fileType);
 		this.dh.updateData();
