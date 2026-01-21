a17d123c736102b5e37f8d38f625085d5e60532e
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-21 00:49:10 +0000

    added a context class to hold datas and future data updates

---
+++ FileManager/src/main/java/FileManager/FileManager/Context.java
@@ -0,0 +1,8 @@
+package FileManager.FileManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+	public Map<String, Integer> dimension = new HashMap<String, Integer>();
+}
