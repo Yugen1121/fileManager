1a20cf8e788d93563946617f0c16239449e285ea
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-22 13:24:48 +0000

    added a class to get the javafx component from fxml

---
+++ FileManager/src/main/java/FileManager/FileManager/Controller.java
@@ -0,0 +1,19 @@
+package FileManager.FileManager;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+	@FXML
+	public VBox right;
+	@FXML
+	public VBox left;
+	@FXML
+	public VBox rightTop;
+	@FXML
+	public VBox rightBottom;
+	@FXML
+	public VBox leftTop;
+	@FXML
+	public VBox leftBottom;
+
+}
