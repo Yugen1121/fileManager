67b88e4447bd54e8000995f74303d5a59cc1317b
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-22 13:23:35 +0000

    removed context

--- FileManager/src/main/java/FileManager/FileManager/Context.java
+++
@@ -1,8 +0,0 @@
-package FileManager.FileManager;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Context {
-	public Map<String, Integer> dimension = new HashMap<String, Integer>();
-}
