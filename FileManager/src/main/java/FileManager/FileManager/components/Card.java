7bda6685e41121fbcd3a4e7ce68dd8139f1f053c
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-22 13:25:53 +0000

    removed everything related to context

--- FileManager/src/main/java/FileManager/FileManager/components/Card.java
+++ FileManager/src/main/java/FileManager/FileManager/components/Card.java
@@ -1,9 +1,12 @@
 package FileManager.FileManager.components;
 
+import FileManager.FileManager.Context;
 import FileManager.FileManager.FileWatcher;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -15,13 +18,19 @@ public class Card{
 	private VBox mainSection;
 	private FileWatcher fw;
 	private VBox root;
+	private Context context;
+	private int height = 250;
+	private int width = 500;
+	
 	
 	public Card(FileWatcher fw, VBox right) { 
 		this.root = new VBox();
+		this.root.getStyleClass().add("filePathBox");
 		this.fw = fw;
 		this.mainSection = right; 
 		build();
 	}
+
 	
 	public VBox getRoot() {
 		return this.root;
@@ -45,7 +54,7 @@ public class Card{
 		});
 		ObservableMap<String, String> map = fw.getFilePaths(); 
 		for (String k: map.keySet()) {
-			mainSection.getChildren().add(new Label(k+": "+map.get(k)));
+			mainSection.getChildren().add(buildFilePathCard(k, map.get(k)));
 		}
 		TextField fileTypeInput = new TextField();
 		TextField filePath  = new TextField();
@@ -53,9 +62,39 @@ public class Card{
 		mainSection.getChildren().add(filePath);
 		Button btn = new Button("Submit");
 		btn.setOnAction(e -> {
-			fw.addFilePath(fileTypeInput.getText(), filePath.getText());
+			try {
+				fw.addFilePath(fileTypeInput.getText(), filePath.getText());
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				System.out.println(e1.getMessage());
+			}
 		});
 		mainSection.getChildren().add(btn);
+	}
+	
+	private HBox buildFilePathCard(String key, String path) {
+		HBox root = new HBox();
+		root.setPrefWidth(width);
+		Label k = new Label(key+": ");
+		Label p = new Label(path);
+		Button btn = new Button("delet");
+		btn.setOnAction(e -> {
+			this.fw.removeFilePath(key);
+		});
+		ScrollPane sk = new ScrollPane(k);
+		ScrollPane sp = new ScrollPane(p);
+		sk.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		sk.setFitToHeight(true);
+		sp.setFitToHeight(true);
+		sk.setPrefWidth(width*0.2);
+		sp.setPrefWidth(width*0.5);
+		btn.setPrefWidth(width*0.3);
+		root.getChildren().add(sk);
+		root.getChildren().add(sp);
+		root.getChildren().add(btn);
+		return root;
+		
 	}
 	
 }
