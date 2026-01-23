de364be7561ebceb947798fc155745029a445fe9
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-23 16:10:23 +0000

    made an environment class that holds all the data

---
+++ FileManager/src/main/java/FileManager/FileManager/Env.java
@@ -0,0 +1,23 @@
+package FileManager.FileManager;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+
+public class Env {
+	private DataHandler data;
+	private ObservableMap<String, FileWatcher> watchers = FXCollections.observableHashMap();
+	public Env(DataHandler data) {
+		this.data = data;
+		for (String s: data.getData().keySet()) {
+			this.watchers.put(s, new FileWatcher(s, this));
+		}
+	}
+	
+	public DataHandler getData() {
+		return this.data;
+	}
+	
+	public ObservableMap<String, FileWatcher> getWatchers(){
+		return this.watchers;
+	}
+}
