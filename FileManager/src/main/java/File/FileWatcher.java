ec1ac398d08faec80c7565c628b17b6c88c82847
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-28 16:55:21 +0000

    .

--- FileManager/src/main/java/File/FileWatcher.java
+++ FileManager/src/main/java/File/FileWatcher.java
@@ -34,16 +34,13 @@ public class FileWatcher {
 						} catch (Exception e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
-						}
-						
-						
+						}	
 					}
 					
 					default:
 						throw new IllegalArgumentException("Unexpected value: ");
 					}
 				}).build();
-
 	}
 	
 	
