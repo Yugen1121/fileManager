ceded662a98782448cecd6ac68852eacdb158f9a
Yugen1121 <yugenlimbu777@gmail.com>
2026-01-28 16:42:35 +0000

    added a event listener to add new filewatchre or remove if the main data changes

--- FileManager/src/main/java/FileManager/FileManager/Env.java
+++ FileManager/src/main/java/FileManager/FileManager/Env.java
@@ -2,6 +2,7 @@ package FileManager.FileManager;
 
 import File.FileWatcher;
 import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableMap;
 
 public class Env {
@@ -10,12 +11,36 @@ public class Env {
 	public Env(DataHandler data) {
 		this.data = data;
 		for (String s: data.getData().keySet()) {
-			this.watchers.put(s, new FileWatcher(s, this));
+			try {
+				this.watchers.put(s, new FileWatcher(s, data.getDirectoryConfig(s)));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 		}
+		
+		data.getData().addListener((MapChangeListener<String, DirectoryConfig>) change -> {
+			String key = change.getKey();
+			try {
+				if (change.wasAdded()) {
+					this.watchers.put(key, new FileWatcher(key, data.getDirectoryConfig(key)));
+				}
+				else if (change.wasRemoved()) {
+					this.removeWatcher(key);
+				}
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		});
 	}
 	
 	public DataHandler getData() {
 		return this.data;
+	}
+	
+	public void removeWatcher(String key) throws Exception {
+		this.watchers.get(key).closeWatcher();
+		this.watchers.remove(key);
 	}
 	
 	public ObservableMap<String, FileWatcher> getWatchers(){
